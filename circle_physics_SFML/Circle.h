05e71ffd59d3d40a76010ed493641bdb981c7c69
Elijah Lawal <elijahlawal26@outlook.com>
2024-06-02 18:59:40 +0100

    Change to world border logic

--- circle_physics_SFML/Circle.h
+++ circle_physics_SFML/Circle.h
@@ -66,25 +66,45 @@ public:
         Collision(allCircles, numCircles, dt);
         Collision(allCircles, numCircles, dt);
         Collision(allCircles, numCircles, dt);
-        pos = { clamp(pos.x, worldSizeX - rad * 2, (rad / 4)), clamp(pos.y, worldSizeY - rad * 2, (rad / 2)) };
+        if (pos.x > worldSizeX - rad * 2.0f)
+        {
+            pos.x = worldSizeX - rad * 2.0f;
+            acc.x = -0.05f;
+        }
+        else if (pos.x < rad / 4.0f)
+        {
+            pos.x = rad / 4.0f;
+            acc.x = 0.05f;
+        }
+
+        if (pos.y > worldSizeY - rad * 2.0f)
+        {
+            pos.y = worldSizeY - rad * 2.0f;
+            acc.y = -0.05f;
+        }
+        else if (pos.y < rad / 2.0f)
+        {
+            pos.y = rad / 2.0f;
+            acc.y = 0.05f;
+        }
 	}
     Circle()
         : pos({ 0, 0 }), posold({ 0, 0 }), acc({ 0, 0 }), rad(25) {}
-    Circle(Vector pos0, int radius)
-        : pos(pos0), posold(pos0), acc({0, 0}), rad(radius) {}
-    Circle(Vector pos0, Vector vel, int radius)
-        : pos(pos0), posold(pos0 + vel), acc({ 0, 0 }), rad(radius) {}
+    Circle(Vector pos0, int rad)
+        : pos(pos0), posold(pos0), acc({0, 0}), rad(rad) {}
+    Circle(Vector pos0, Vector vel, int rad)
+        : pos(pos0), posold(pos0 + vel), acc({ 0, 0 }), rad(rad) {}
 };
 
 
 
-void CollisionNoSelf(Circle allCircles[], int numCircles, float dt, Vector source, int radius)
+void CollisionNoSelf(Circle allCircles[], int numCircles, float dt, Vector source, int rad)
 {
     for (int i = 0; i < numCircles; i++)
     {
         Vector axis = source - allCircles[i].pos;
         float dist = axis.mag();
-        float minDist = radius + allCircles[i].rad;
+        float minDist = rad + allCircles[i].rad;
         if (dist <= minDist)
         {
             Vector n = axis / dist;
